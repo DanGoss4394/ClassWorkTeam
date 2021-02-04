5880f6e51f016c7de9a9487a391ee85d1cac4ef7
Ben <benriveracoding@gmail.com>
2021-02-04 15:49:39 -0700

    string function to capitalize first letters in a string

--- src/helper_functions/string_helper.js
+++ src/helper_functions/string_helper.js
@@ -1,2 +1,8 @@
-//  String
-// Ben & Brandon
+export const capitalizer = (sentence) => {
+    const divSen = sentence.split(" ")
+    return divSen.map(word => {
+    return word.charAt(0).toUpperCase() + word.slice(1)
+    }).join(" ")
+}
+
+console.log(capitalizer("this sentance should return a string of capitalized words"))
